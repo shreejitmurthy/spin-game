18d34d0adaf2d92410078b53d516d3e3dfb16994
Shreejit Murthy <botmorph17@gmail.com>
2025-01-16 01:06:58 +0530

    implemented camera movement with mouse

--- src/camera.c
+++ src/camera.c
@@ -1,14 +1,17 @@
 #include "camera.h"
-
-#include <SDL3/SDL_keycode.h>
 #include "keyboard.h"
+
+#include <log/log.h>
 
 camera_t camera_init(vec3 position, vec3 target, vec3 up) {
     camera_t camera;
     glm_vec3_copy(position, camera.position);
     glm_vec3_copy(target, camera.target);
     glm_vec3_copy(up, camera.up);
-    glm_vec3_copy((vec3){0.0f, 0.0f, -1.0f}, camera.front);
+    camera.yaw = -90.0f;
+    camera.pitch = 0.0f;
+
+    glm_vec3_copy((vec3){0.0f, 0.0f, 1.0f}, camera.front);
     glm_lookat(camera.position, camera.target, camera.up, camera.view);
     return camera;
 }
@@ -16,6 +19,9 @@ camera_t camera_init(vec3 position, vec3 target, vec3 up) {
 void camera_move(camera_t* camera, vec3 movement, float delta_time) {
     vec3 delta;
     glm_vec3_scale(movement, camera->speed * delta_time, delta);
+
+    if (!camera->free_cam) delta[1] = 0.f;  // restrict movement in y-dir unless free cam is enabled
+
     glm_vec3_add(camera->position, delta, camera->position);
     glm_vec3_add(camera->target, delta, camera->target);
 }
@@ -37,3 +43,27 @@ void camera_handle_input(camera_t* camera, controls_t controls, float delta_time
 
     camera_move(camera, movement, delta_time);
 }
+
+void camera_handle_mouse(camera_t *camera, float x_rel, float y_rel) {
+    x_rel = glm_clamp(x_rel, -500.f, 500.f);
+    y_rel = glm_clamp(y_rel, -500.f, 500.f);
+
+    camera->yaw += x_rel * camera->sensitivity;
+    // need to invert the y value
+    camera->pitch -= y_rel * camera->sensitivity;
+
+    if (camera->pitch > 89.0f)
+        camera->pitch = 89.0f;
+    if (camera->pitch < -89.0f)
+        camera->pitch = -89.0f;
+
+    vec3 front;
+    front[0] = cosf(glm_rad(camera->yaw)) * cosf(glm_rad(camera->pitch));
+    front[1] = sinf(glm_rad(camera->pitch));
+    front[2] = sinf(glm_rad(camera->yaw)) * cosf(glm_rad(camera->pitch));
+    glm_vec3_normalize_to(front, camera->front);
+
+    glm_vec3_add(camera->position, camera->front, camera->target);
+    glm_lookat(camera->position, camera->target, camera->up, camera->view);
+}
+
--- src/camera.h
+++ src/camera.h
@@ -1,9 +1,9 @@
 #pragma once
 
 #include <cglm/cglm.h>
+#include <stdbool.h>
 
 #include "controls.h"
-
 
 typedef struct camera_t {
     vec3 position;
@@ -16,8 +16,10 @@ typedef struct camera_t {
     float yaw;
     float pitch;
     float sensitivity;
+    bool free_cam;
 } camera_t;
 
 camera_t camera_init(vec3 position, vec3 target, vec3 up);
 void camera_move(camera_t* camera, vec3 movement, float delta_time);
 void camera_handle_input(camera_t* camera, controls_t controls, float delta_time);
+void camera_handle_mouse(camera_t* camera, float x_rel, float y_rel);
