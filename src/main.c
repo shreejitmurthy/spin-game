cada18e95b55338b108c1d1e606e3823ed521887
Shreejit Murthy <botmorph17@gmail.com>
2025-01-16 10:13:49 +0530

    reflect changes

--- src/main.c
+++ src/main.c
@@ -10,183 +10,17 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 #include <glad/glad.h>
-#define STB_IMAGE_IMPLEMENTATION
-#include <stb_image.h>
 #include <cglm/cglm.h>
 
 #include "camera.h"
 #include "keyboard.h"
 #include "controls.h"
 #include "shader.h"
+#include "texture.h"
+
+#include <stb_image.h>
 
 #include "enemy.img.h"
-
-typedef struct texture_t {
-    GLuint id;
-    GLuint vbo, vao, ebo;
-    uint32_t width, height, nr_channels;
-} texture_t;
-
-typedef struct quad_t {
-    float x, y, w, h;
-} quad_t;
-
-texture_t load_texture_raw(const uint32_t img_data[], uint32_t width, uint32_t height) {
-    texture_t texture;
-    glGenTextures(1, &texture.id);
-    glBindTexture(GL_TEXTURE_2D, texture.id);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-    const unsigned char* data = (const unsigned char*)img_data;
-    texture.nr_channels = 4;
-
-    if (data) {
-        glEnable(GL_BLEND);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    } else {
-        log_error("Failed to read pixel data");
-    }
-
-    texture.width = width;
-    texture.height = height;
-
-    quad_t quad = (quad_t){0, 0, (float)width, (float)height};
-
-    float u0 = quad.x / width;
-    float v0 = quad.y / height;
-    float u1 = (quad.x + quad.w) / width;
-    float v1 = (quad.y + quad.h) / height;
-
-    float halfWidth  = quad.w / 2.0f;
-    float halfHeight = quad.h / 2.0f;
-
-    float vertices[32] = {
-        /*   positions         tx coords        colors     */
-         halfWidth,  halfHeight,  0.0f,   u1, v1,    1.0f, 0.0f, 0.0f,  // top right
-         halfWidth, -halfHeight,  0.0f,   u1, v0,    0.0f, 1.0f, 0.0f,  // bottom right
-        -halfWidth, -halfHeight,  0.0f,   u0, v0,    0.0f, 0.0f, 1.0f,  // bottom left
-        -halfWidth,  halfHeight,  0.0f,   u0, v1,    1.0f, 1.0f, 0.0f   // top left
-    };
-
-    unsigned int indices[6] = {
-            0, 1, 3,  // first triangle
-            1, 2, 3   // second triangle
-    };
-
-    glGenVertexArrays(1, &texture.vao);
-    glGenBuffers(1, &texture.vbo);
-    glGenBuffers(1, &texture.ebo);
-
-    glBindVertexArray(texture.vao);
-
-    glBindBuffer(GL_ARRAY_BUFFER, texture.vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, texture.ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);           // position (3 floats)
-    glEnableVertexAttribArray(0);
-
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float))); // texture coords (2 floats)
-    glEnableVertexAttribArray(1);
-
-    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(5 * sizeof(float))); // color (3 floats)
-    glEnableVertexAttribArray(2);
-
-    glBindVertexArray(0);
-
-    return texture;
-}
-
-texture_t load_texture(const char* filename) {
-    texture_t texture;
-    glGenTextures(1, &texture.id);
-    glBindTexture(GL_TEXTURE_2D, texture.id);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-    const unsigned char* data = stbi_load(filename, &texture.width, &texture.height, &texture.nr_channels, 4);
-
-    if (data) {
-        glEnable(GL_BLEND);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texture.width, texture.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    } else {
-        log_error("Failed to read pixel data");
-    }
-
-    int width = texture.width;
-    int height = texture.height;
-
-    quad_t quad = (quad_t){0, 0, (float)width, (float)height};
-
-    float u0 = quad.x / width;
-    float v0 = quad.y / height;
-    float u1 = (quad.x + quad.w) / width;
-    float v1 = (quad.y + quad.h) / height;
-
-    float halfWidth  = quad.w / 2.0f;
-    float halfHeight = quad.h / 2.0f;
-
-    float vertices[32] = {
-        /*   positions         tx coords        colors     */
-         halfWidth,  halfHeight,  0.0f,   u1, v1,    1.0f, 0.0f, 0.0f,  // top right
-         halfWidth, -halfHeight,  0.0f,   u1, v0,    0.0f, 1.0f, 0.0f,  // bottom right
-        -halfWidth, -halfHeight,  0.0f,   u0, v0,    0.0f, 0.0f, 1.0f,  // bottom left
-        -halfWidth,  halfHeight,  0.0f,   u0, v1,    1.0f, 1.0f, 0.0f   // top left
-    };
-
-    unsigned int indices[6] = {
-            0, 1, 3,  // first triangle
-            1, 2, 3   // second triangle
-    };
-
-    glGenVertexArrays(1, &texture.vao);
-    glGenBuffers(1, &texture.vbo);
-    glGenBuffers(1, &texture.ebo);
-
-    glBindVertexArray(texture.vao);
-
-    glBindBuffer(GL_ARRAY_BUFFER, texture.vbo);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, texture.ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);           // position (3 floats)
-    glEnableVertexAttribArray(0);
-
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float))); // texture coords (2 floats)
-    glEnableVertexAttribArray(1);
-
-    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(5 * sizeof(float))); // color (3 floats)
-    glEnableVertexAttribArray(2);
-
-    glBindVertexArray(0);
-}
-
-void draw_texture(texture_t texture) {
-    glBindTexture(GL_TEXTURE_2D, texture.id);
-    glBindVertexArray(texture.vao);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-}
-
-void delete_texture(texture_t texture) {
-    glDeleteVertexArrays(1, &texture.vao);
-    glDeleteBuffers(1, &texture.vbo);
-    glDeleteBuffers(1, &texture.ebo);
-}
 
 uint64_t last_time = 0;
 
@@ -275,9 +109,7 @@ int main() {
                 }
             }
             if (event.type == SDL_EVENT_MOUSE_MOTION) {
-                log_info("Mouse rel coords: (%.2f, %.2f)", event.motion.xrel, event.motion.yrel);
                 camera_handle_mouse(&camera, event.motion.xrel, event.motion.yrel);
-
             }
         }
 
